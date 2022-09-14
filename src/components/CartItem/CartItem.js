084b138ab4c512b9248cb95dacd5ec9fb47aa037
facSav <facu.savio@gmail.com>
2022-09-14 00:12:02 -0300

    desafio contexto

--- src/components/CartItem/CartItem.js
+++ src/components/CartItem/CartItem.js
@@ -1,18 +1,18 @@
 
 
-export function CartItem (infoDetalle) {
+export function CartItem ({infoDetalle}) {
     return(
         
-        <div class="card mb-3" style="max-width: 540px;">
+        <div class="card mb-3" style={{maxWidth:"540px"}}>
             <div class="row g-0">
-                <div class="col-md-4">
+                <div class="col-md-4 ">
                     <img src={infoDetalle.foto} class="img-fluid rounded-start" alt="..."/>
                 </div>
                 <div class="col-md-8">
                     <div class="card-body">
                         <h5 class="card-title">{infoDetalle.titulo}</h5>
-                        <p class="card-text">pendiente</p>
-                        <p class="card-text"><small class="text-muted">aaaaa</small></p>
+                        <p class="card-text">{`Cantidad: ${infoDetalle.quantity}`}</p>
+                        <button className="btn btn-primary">Eliminar del Carrito</button>
                     </div>
                 </div>
             </div>
