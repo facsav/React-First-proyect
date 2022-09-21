e2bfc4a51ea3409a1361a0be110bc9cc59b116fc
facSav <facu.savio@gmail.com>
2022-09-21 20:30:02 -0300

    firebase

--- src/components/ItemDetailContainer/ItemDetailContainer.js
+++ src/components/ItemDetailContainer/ItemDetailContainer.js
@@ -4,7 +4,8 @@ import { ItemDetail } from "../ItemDetail/ItemDetail"
 import { useParams } from "react-router-dom"
 import { getRoles } from "@testing-library/react"
 import { arrayItems } from "../../helper/helper"
-
+import {doc, getDoc} from 'firebase/firestore'
+import {dataBase} from '../../utils/firebase'
 
 export function ItemDetailContainer (){
     
@@ -14,20 +15,28 @@ export function ItemDetailContainer (){
 
     const [item, setItem] = useState({});
 
-    const getItem = (id)=>{
-        return new Promise((resolve, reject)=>{
-            const product = arrayItems.find(item=>item.id === parseInt(id));
-            resolve(product)
-        })
-    }
+    // const getItem = (id)=>{
+    //     return new Promise((resolve, reject)=>{
+    //         const product = arrayItems.find(item=>item.id === parseInt(id));
+    //         resolve(product)
+    //     })
+    // }
 
     useEffect(()=>{
-        const getProducto = async()=>{
-            const producto = await getItem(id);
+        const queryRef = doc(dataBase, "items", id)
+        getDoc(queryRef).then(response=>{
+            const newDoc={
+                ...response.data(),
+                id:response.id
+            }
+            setItem(newDoc)
+        })
+        // const getProducto = async()=>{
+        //     const producto = await getItem(id);
 
-            setItem(producto);
-        }
-        getProducto();
+        //     setItem(producto);
+        // }
+        // getProducto();
     },[id])
 
 
